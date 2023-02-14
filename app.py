d07ad2b0c9a95cf1be75096af62991921358ed6b
UltroA <64162002+UltroA@users.noreply.github.com>
2023-02-14 16:16:59 +0300

    Added comments and deleted files

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+# import flask-needed from flask library
 from flask import Flask, render_template, g
+# import db library
 import sqlite3
+# App's API (additional script for using less code in main file)
 import API
 
 app = Flask(__name__)
 
-text = ['adsfasdfasdfasdf\n', 'asdfasdfasdf\n', 'adfasdfasdfasdfasdfa\n']
-
+# Routes on website
 @app.route('/')
 def comments():
+    # main page with posts
+    # we need to make more mobile optimization
     comments = [['Shitpost.', 'name', '/static/ТГ.png'],
                 ['Some text', 'more text', '/static/Картина.png'],
                 ['Kill your family - dekma', 'Лучший исполнитель', '/static/Дед.png'],
@@ -15,5 +19,8 @@ def comments():
     return render_template('comments.html', comments=comments)
 
 
+# TODO: work with database
+
+# main site working cycle
 if __name__ == '__main__':
     app.run(port=9000)
